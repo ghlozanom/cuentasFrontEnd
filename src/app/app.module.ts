ffd5e1c8cd4ae7b4c6057efa86c1c52cf6eb63bc
glozano <ghlm81@hotmail.com>
2019-08-02 11:32:44 +0200

    First version of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http'; 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { 
+  MdcFabModule,
+  MdcIconModule,
+  MdcMenuModule,
+  MdcButtonModule,
+  MdcListModule,
+  MdcSnackbarModule,
+  MdcDialogModule
+} from '@angular-mdc/web';
+import { RegistriesComponent } from './components/registries/registries.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdcFabModule,
+    MdcIconModule,
+    MdcMenuModule,
+    MdcButtonModule,
+    MdcListModule,
+    MdcSnackbarModule,
+    MdcDialogModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
